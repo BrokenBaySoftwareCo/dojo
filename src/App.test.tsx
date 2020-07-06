812f3acc24437f969a1effd2f7ff2bf1f38cc49c
Guyana <guyanah@gmail.com>
2020-07-06 01:35:42 -0600

    fixed the test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -36,11 +36,11 @@ it("should display anagrams of a third input", () => {
   const { getByText, getByLabelText } = render(<App />);
 
   const nameInput = getByLabelText(/name/i);
-  fireEvent.change(nameInput, { target: { value: "ef" } });
+  fireEvent.change(nameInput, { target: { value: "xy" } });
 
   fireEvent.click(getByText(/submit/i));
 
-  const expectedAnagrams = ["ef", "fe"];
+  const expectedAnagrams = ["xy", "yx"];
 
   expectedAnagrams.forEach((anagram) => {
     expect(document.body.innerHTML).toMatch(anagram);
